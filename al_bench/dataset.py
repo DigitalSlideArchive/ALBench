989f9180597d41755330020ee94a5966ec3d3494
Lee Newberg <lee.newberg@kitware.com>
2022-04-07 12:53:37 -0400

    ENH: First implementation of dataset.py

--- al_bench/dataset.py
+++ al_bench/dataset.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class Dataset:
-    def __init__():
+    def __init__(self):
         self.features = None
         self.labels = None
         self.dictionaries = None
@@ -13,15 +13,15 @@ class Dataset:
     Handle the vector of features for each stored entity.
     """
 
-    def read_all_features_from_h5py(filename, data_name="features"):
+    def read_all_features_from_h5py(self, filename, data_name="features"):
         with h5.File(filename) as ds:
             self.features = np.array(ds[data_name])
 
-    def write_all_features_to_h5py(filename, data_name="features"):
+    def write_all_features_to_h5py(self, filename, data_name="features"):
         with h5.File(filename, "w") as f:
             f.create_dataset(data_name, self.features.shape, data=self.features)
 
-    def set_all_features(features):
+    def set_all_features(self, features):
         if isinstance(features, np.ndarray) and len(features.shape) == 2:
             self.features = features
         else:
@@ -29,13 +29,13 @@ class Dataset:
                 "The argument to set_all_features must be a 2-dimensional numpy ndarray."
             )
 
-    def get_all_features():
+    def get_all_features(self):
         return self.features
 
-    def clear_all_features():
+    def clear_all_features(self):
         self.features = None
 
-    def set_some_features(feature_indices, features):
+    def set_some_features(self, feature_indices, features):
         """
         This overwrites existing features.
 
@@ -47,7 +47,7 @@ class Dataset:
         # This does not (yet) handle insert, delete, or append.
         self.features[feature_indices,] = features
 
-    def get_some_features(feature_indices):
+    def get_some_features(self, feature_indices):
         """
         N.B. as with numpy arrays in general, a single feature index and a list with one
         feature index will have different behaviors, in that the former drops an array
@@ -59,15 +59,15 @@ class Dataset:
     Handle the label(s) for each stored entity.
     """
 
-    def read_all_labels_from_h5py(filename, data_name="labels"):
+    def read_all_labels_from_h5py(self, filename, data_name="labels"):
         with h5.File(filename) as ds:
             self.labels = np.array(ds[data_name])
 
-    def write_all_labels_to_h5py(filename, data_name="labels"):
+    def write_all_labels_to_h5py(self, filename, data_name="labels"):
         with h5.File(filename, "w") as f:
             f.create_dataset(data_name, self.labels.shape, data=self.labels)
 
-    def set_all_labels(labels):
+    def set_all_labels(self, labels):
         if isinstance(labels, np.ndarray) and 1 <= len(labels.shape) <= 2:
             self.labels = labels
         else:
@@ -76,13 +76,13 @@ class Dataset:
                 "numpy ndarray."
             )
 
-    def get_all_labels():
+    def get_all_labels(self):
         return self.labels
 
-    def clear_all_labels():
+    def clear_all_labels(self):
         self.labels = None
 
-    def set_some_labels(label_indices, labels):
+    def set_some_labels(self, label_indices, labels):
         """
         This overwrites existing labels.
 
@@ -95,7 +95,7 @@ class Dataset:
 
         self.labels[label_indices,] = labels
 
-    def get_some_labels(label_indices):
+    def get_some_labels(self, label_indices):
         """
         N.B. as with numpy arrays in general, a single label index and a list with one
         label index will have different behaviors, in that the former drops an array
@@ -107,7 +107,7 @@ class Dataset:
     Handle the dictionary of supplemental information for each stored entity.
     """
 
-    def set_all_dictionaries(dictionaries):
+    def set_all_dictionaries(self, dictionaries):
         """
         N.B. if we were handed a (read-only) tuple then we cannot change a subset of
         them.  If this functionality is needed then supply `list(dictionaries)` to this
@@ -125,13 +125,13 @@ class Dataset:
                 "of dictionaries."
             )
 
-    def get_all_dictionaries():
+    def get_all_dictionaries(self):
         return self.dictionaries
 
-    def clear_all_dictionaries():
+    def clear_all_dictionaries(self):
         self.dictionaries = None
 
-    def set_some_dictionaries(dictionary_indices, dictionaries):
+    def set_some_dictionaries(self, dictionary_indices, dictionaries):
         """
         This overwrites existing dictionaries.
 
@@ -152,7 +152,7 @@ class Dataset:
         # This does not (yet) handle insert, delete, or append.
         self.dictionaries[dictionary_indices,] = dictionaries
 
-    def get_some_dictionaries(dictionary_indices):
+    def get_some_dictionaries(self, dictionary_indices):
         """
         N.B. as with numpy arrays in general, a single dictionary index and a list with
         one dictionary index will have different behaviors, in that the former drops an
@@ -165,7 +165,7 @@ class Dataset:
     supplemental information of each stored entity.
     """
 
-    def set_all_label_definitions(label_definitions):
+    def set_all_label_definitions(self, label_definitions):
         """
         The argument is, for example, label_definitions = {
             np.nan: {"description": "unlabeled", "color": "#FFFF00"},
@@ -177,10 +177,10 @@ class Dataset:
         if isinstance(label_definitions, dict):
             self.label_definitions = label_definitions
 
-    def get_all_label_definitions():
+    def get_all_label_definitions(self):
         return self.label_definitions
 
-    def check_data_consistency():
+    def check_data_consistency(self):
         """
         Among features, labels, and dictionaries that were supplied, are they for the
         same number of entities?
