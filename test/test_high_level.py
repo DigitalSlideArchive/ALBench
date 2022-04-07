8a1b99490428a742d1f2217b7ebfd9c6bfe7d34b
Lee Newberg <lee.newberg@kitware.com>
2022-04-07 12:55:52 -0400

    ENH: Add first test

---
+++ test/test_high_level.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Copyright NumFOCUS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def test_imports():
+    """Purpose: Test that needed packages are available"""
+
+    # Import succeeds
+    import al_bench as alb
+    import h5py as h5
+    import numpy as np
+
+
+def test_high_level():
+    """Purpose: Test that high-level operations work"""
+    import al_bench as alb
+
+    my_dataset = alb.dataset.Dataset()
+    my_model = alb.model.PyTorchModel()
+    my_strategy = alb.strategy.Strategy()
+    my_strategy.set_model(my_model)
+    assert my_strategy.get_model() is my_model
+    my_strategy.set_dataset(my_dataset)
+    assert my_strategy.get_dataset() is my_dataset
